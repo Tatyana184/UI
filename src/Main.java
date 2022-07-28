e2080e4edc620a1fb29ab4c6b7513fedd8b0eda8
Tatyana184 <tatianachesss@yandex.ru>
2022-07-28 17:31:42 +0500

    changeSize

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
         JLabel label = new JLabel("Моя первая надпись!");
-        label.setFont(new Font("Verdana", Font.PLAIN, 20));
         label.setVerticalAlignment(JLabel.TOP);
 
-        JOptionPane.showMessageDialog(frame,label);
+        String[] size = {"10","20","30","40"};
+        JComboBox comboBox = new JComboBox(size);
+         int check = JOptionPane.showConfirmDialog(
+                frame,  comboBox, "Выберите размер шрифта",JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+    if(check == JOptionPane.OK_OPTION) {
+
+
+            int sizeText = Integer.parseInt((String) comboBox.getSelectedItem());
+
+            label.setFont(new Font("Verdana", Font.PLAIN, sizeText));
+
+}
+        JOptionPane.showMessageDialog(frame, label);
     }
 }
