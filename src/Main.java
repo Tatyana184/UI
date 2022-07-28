1df7e45104165fbedaf8cc93df0a2073314fd5d4
Tatyana184 <tatianachesss@yandex.ru>
2022-07-28 22:38:12 +0500

    Spinner

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,32 @@ public class Main {
 
    static int count = 0;
     public static void main(String[] args) {
+        JFrame f= new JFrame();
 
-        JFrame frame = new JFrame();
-        JLabel label = new JLabel("Количество нажатий: " + count);
-        JButton button = new JButton();
-        frame.setSize(200,200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setLayout(new BorderLayout());
+        f.setVisible(true);
+        f.setSize(200,200);
+        Container c = f.getContentPane();
+        c.setLayout(null);
+        JButton btn=new JButton("Ответить ");
+        JLabel label = new JLabel();
+        String[] num = {"1","2","3","4","5","6","7","8","9","10"};
+        JSpinner spinner = new JSpinner(new SpinnerListModel(num));
+
+        spinner.setBounds(40,10,100,20);
+        btn.setBounds(40,50,100,20);
+        label.setBounds(50,100,100,20);
+
+        f.add(btn);
+        f.add(label);
+        f.add(spinner);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-        frame.add(label,BorderLayout.PAGE_START);
-        frame.add(button,BorderLayout.CENTER);
-
-        button.addActionListener(new ActionListener() {
+        btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                count += 1;
-                label.setText("Количество нажатий: " + count);
+
+                label.setText("Ответ: " + spinner.getValue());
 
             }
         });
